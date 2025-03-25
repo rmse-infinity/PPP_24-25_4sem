ed3c9b8f29204aaa649feceed026ca0e5f2b1218
rmse_inf <kibernetnortvest@gmail.com>
2025-03-25 16:17:38 +0300

    Update main.py

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,6 +1,22 @@
 
 def main():
-    # Ваш код здесь
+    import subprocess
+    import time
+    import os
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    
+    server_script = os.path.join(script_path, "server", "server.py")
+    client_script = os.path.join(script_path, "klient", "klient.py")
+    
+    server_process = subprocess.Popen(["python3", server_script])
+    
+    time.sleep(1)
+    
+    client_process = subprocess.Popen(["python3", client_script])
+    
+    server_process.wait()
+    client_process.wait()
+
     pass
 
 if __name__ == "__main__":
